7360cdea1cd6d28d94714bee97eebf93ca851001
unknown <=>
2018-05-08 15:58:50 -0700

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  itemCount: Number = 4;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
